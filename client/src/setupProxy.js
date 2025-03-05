2489df5ea88de5d3d466f909469967717d75f9e3
Ahmad Ayoub <matoayoub2002@gmail.com>
2025-03-05 19:05:33 +0200

    modified:   client/src/setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+console.log("setupProxy.js is being executed!");
+
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000', // Your Express server URL
+            target: 'http://localhost:5000',
             changeOrigin: true,
         })
     );
-};      
+
+    console.log("Proxy middleware set up!");
+};
