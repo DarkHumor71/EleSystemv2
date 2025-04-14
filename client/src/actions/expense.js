dfc13bfa094c8ecec20146b6ba00c2e2eac5ffa8
Darkhumor71 <moudyoussef1@gmail.com>
2025-04-14 12:01:34 +0300

    created expense fetch for apartment

--- client/src/actions/expense.js
+++ client/src/actions/expense.js
@@ -1,15 +1,29 @@
-import {FETCHBUILDINGEXPENSE} from "./types";
-import axios from "axios";
-import setAuthToken from "../utils/setAuthToken";
+import { FETCHAPARTMENTEXPENSES, FETCHBUILDINGEXPENSE } from './types';
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
 
 export const fetchBuildingExpense = (building) => async (dispatch) => {
-    try {
-        setAuthToken(localStorage.token);
-        const response = await axios.get(`/api/expense/building/${building}`);
-        if (response.data) {
-            dispatch({type: FETCHBUILDINGEXPENSE});
-            return response.data;
-        }
-    } catch (error) {
+  try {
+    setAuthToken(localStorage.token);
+    const response = await axios.get(`/api/expense/building/${building}`);
+    if (response.data) {
+      dispatch({ type: FETCHBUILDINGEXPENSE });
+      return response.data;
     }
+    return [];
+  } catch (error) {}
+};
+export const fetchApartmentExpenses = (apartment_id) => async (dispatch) => {
+  try {
+    setAuthToken(localStorage.token);
+    const response = await axios.get(`/api/expense/apartment/${apartment_id}`);
+    if (response.data) {
+      dispatch({ type: FETCHAPARTMENTEXPENSES });
+      return response.data;
+    }
+
+    return [];
+  } catch (error) {
+    return [];
+  }
 };
