a421bad20dd215a7cdcf1b320f77d1c27d25225d
Darkhumor71 <moudyoussef1@gmail.com>
2025-04-15 18:17:33 +0300

    fixed all routing

--- client/src/components/routing/privateroute.jsx
+++ client/src/components/routing/privateroute.jsx
@@ -1,19 +1,60 @@
-import React from "react";
-import {connect} from "react-redux";
-import PropTypes from "prop-types";
-import {Navigate} from "react-router-dom";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = ({auth: {isAuthenticated, loading}, children}) => {
-    if (!isAuthenticated && !loading) {
-        return <Navigate to="/login"/>;
+const PrivateRoute = ({
+  auth: { isAuthenticated, loading, isResident, isModerator, is_admin },
+  children,
+}) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  // Define role-based whitelists
+  const roleWhitelists = {
+    resident: ['/apr', '/profile'],
+    admin: ['/dash', '/create_building'],
+    moderator: [
+      '/mod',
+      '/apartments',
+      '/create_apartment',
+      '/qrcode',
+      '/expenses',
+    ],
+  };
+  if (loading) return null;
+  if (!isAuthenticated && !loading) {
+    return <Navigate to="/login" />;
+  }
+
+  // Check role and whitelist
+  if (isAuthenticated) {
+    if (is_admin && !roleWhitelists.admin.includes(currentPath)) {
+      return <Navigate to="/dash" />;
     }
-    return children;
+
+    if (
+      isResident &&
+      !isModerator &&
+      !roleWhitelists.resident.includes(currentPath)
+    ) {
+      return <Navigate to="/apr" />;
+    }
+
+    if (isModerator && !roleWhitelists.moderator.includes(currentPath)) {
+      return <Navigate to="/mod" />;
+    }
+  }
+
+  return children;
 };
 
-PrivateRoute.propTypes = {auth: PropTypes.object.isRequired};
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = (state) => ({
-    auth: state.auth,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
