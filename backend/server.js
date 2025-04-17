7a71d6e0f7c2df1f348d1e8ec080b35f2047aa08
Darkhumor71 <moudyoussef1@gmail.com>
2025-04-17 15:25:06 +0300

    refc

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 //Connect Database
 connectDB();
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 //Init Middleware
-app.use(express.json({extended: false})); // to use req.body
+app.use(express.json({ extended: false })); // to use req.body
 app.use(cors());
 //Define Routes
-app.use("/api/auth", require("./routes/API/auth"));
-app.use("/api/building", require("./routes/API/building"));
-app.use("/api/apartment", require("./routes/API/apartment"));
-app.use("/api/expense", require("./routes/API/expense"));
+app.use('/api/auth', require('./routes/API/auth'));
+app.use('/api/building', require('./routes/API/building'));
+app.use('/api/apartment', require('./routes/API/apartment'));
+app.use('/api/expense', require('./routes/API/expense'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () =>
-    console.log(`Example app listening on port ${PORT}!`)
+app.listen(PORT, '127.0.0.1', () =>
+  console.log(`Example app listening on port ${PORT}!`)
 );
