f1bf6968135a883931e3e81c7e6dffd63641ef22
Ahmad Ayoub <matoayoub2002@gmail.com>
2025-04-22 18:44:32 +0300

    added session autoload

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@
 
 const { mongoose } = require('mongoose');
 const config = require('config');
-
+const Session = require('../models/Session');
 // Get the MongoDB connection string from config/default.json or environment
 const db = config.get('mongoURI');
 
@@ -20,9 +20,25 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
     console.log('MongoDB Connected...');
+    resetSessionFields();
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1); // Exit process with failure
+  }
+};
+const resetSessionFields = async () => {
+  try {
+    await Session.findByIdAndUpdate(
+      'singleton',
+      {
+        apartment: null,
+        time: 0,
+      },
+      { upsert: true }
+    );
+    console.log("Session fields reset.");
+  } catch (error) {
+    console.error("Error resetting session fields:", error);
   }
 };
 
