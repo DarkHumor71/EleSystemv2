f35ed576ec067332e87124285d6e8acfe6875ead
Ahmad Ayoub <matoayoub2002@gmail.com>
2025-04-22 18:44:55 +0300

    created session schema

---
+++ backend/models/Session.js
@@ -0,0 +1,40 @@
+/**
+ * @file Session.js
+ * @description Mongoose schema for Session.
+ *
+ * @module models/Session
+ */
+
+const mongoose = require("mongoose");
+
+/**
+ * Session Schema
+ * Represents the electricity usage data (in time, power, and cost) for an apartment.
+ * Supports soft delete functionality.
+ */
+const SessionSchema = new mongoose.Schema(
+    {
+        /**
+         * A fixed ID to ensure this schema always has only one record.
+         * @type {String}
+         */
+        _id: {
+            type: String,
+            default: 'singleton'
+        },
+
+        /**
+         * Reference to the apartment this session belongs to.
+         * @type {mongoose.Types.ObjectId}
+         */
+        apartment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Apartment",
+        },
+    }
+);
+
+/** Session model based on the schema */
+const Session = mongoose.model("Session", SessionSchema);
+
+module.exports = Session;
